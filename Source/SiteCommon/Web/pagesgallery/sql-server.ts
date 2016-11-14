2f509ecd7326287f0113cc55c72ac1bd5eb51d2a
Yasin Shtiui <yashti@microsoft.com>
2016-11-14 11:12:54 -0800

    Fix sql-server page

--- Source/SiteCommon/Web/pagesgallery/sql-server.ts
+++ Source/SiteCommon/Web/pagesgallery/sql-server.ts
@@ -15,7 +15,7 @@ export class SqlServer extends ViewModelBase {
     hideSqlAuth: boolean = false;
     isAzureSql: boolean = false;
     isWindowsAuth: boolean = true;
-   
+
     newSqlDatabase: string = null;
     password: string = '';
     passwordConfirmation: string = '';
@@ -50,10 +50,6 @@ export class SqlServer extends ViewModelBase {
     }
 
     async OnValidate(): Promise<boolean> {
-        if (!super.OnValidate()) {
-            return false;
-        }
-
         this.isValidated = false;
 
         this.sqlServer = this.sqlServer.toLowerCase();
@@ -81,13 +77,16 @@ export class SqlServer extends ViewModelBase {
             }
         }
 
+        let didOtherValidateActionsPass: boolean = await super.OnValidate();
+        this.isValidated = this.isValidated && didOtherValidateActionsPass;
+
         return this.isValidated;
     }
 
     async NavigatingNext(): Promise<boolean> {
 
         let body = this.GetBody(true);
-        let response:ActionResponse = null;
+        let response: ActionResponse = null;
 
         if (this.sqlInstance === 'ExistingSql') {
             response = await this.MS.HttpService.executeAsync('Microsoft-GetSqlConnectionString', body);
