ad33e462c2f8509a85dc94cf480cf3063a4197ab
Yasin Shtiui <yashti@microsoft.com>
2016-11-18 12:00:45 -0800

    Suppress CS1998 pragma warning globally

--- Source/Common/Microsoft.Deployment.Common.Test/DummyActions/MockAction.cs
+++ Source/Common/Microsoft.Deployment.Common.Test/DummyActions/MockAction.cs
@@ -9,9 +9,7 @@ namespace Microsoft.Deployment.Common.Test.DummyActions
     [Export(typeof(IAction))]
     public class MockAction : BaseAction
     {
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
         public override async Task<ActionResponse> ExecuteActionAsync(ActionRequest request)
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
             return new ActionResponse(ActionStatus.Success, "Hello"); 
         }
