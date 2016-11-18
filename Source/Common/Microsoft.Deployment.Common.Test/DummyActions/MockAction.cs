e63c9beba9599f0c10d3e2963f3ede93c28921b0
Yasin Shtiui <yashti@microsoft.com>
2016-11-18 11:38:46 -0800

    Ignore an async warning for a mock action test

--- Source/Common/Microsoft.Deployment.Common.Test/DummyActions/MockAction.cs
+++ Source/Common/Microsoft.Deployment.Common.Test/DummyActions/MockAction.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.ComponentModel.Composition;
 using System.Threading.Tasks;
+
 using Microsoft.Deployment.Common.ActionModel;
 using Microsoft.Deployment.Common.Actions;
-using System.ComponentModel.Composition;
 
 namespace Microsoft.Deployment.Common.Test.DummyActions
 {
     [Export(typeof(IAction))]
     public class MockAction : BaseAction
     {
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
         public override async Task<ActionResponse> ExecuteActionAsync(ActionRequest request)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
             return new ActionResponse(ActionStatus.Success, "Hello"); 
         }
