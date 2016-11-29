1d6710f6bb0a7cc16affa667dd13f3f5efc180a3
Justyna Lucznik <juluczni@microsoft.com>
2016-11-28 17:07:56 -0800

    Added actions and tests for Logic app connectors. Created skeleton for the News init.json file.

---
+++ Source/Actions/Microsoft.Deployment.Actions.AzureCustom/Common/CreateConnectorToLogicApp.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.Composition;
+using System.Dynamic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.Azure.Management.Resources;
+using Microsoft.Deployment.Common;
+using Microsoft.Deployment.Common.ActionModel;
+using Microsoft.Deployment.Common.Actions;
+using Microsoft.Deployment.Common.ErrorCode;
+using Microsoft.Deployment.Common.Helpers;
+
+namespace Microsoft.Deployment.Actions.AzureCustom.Common
+{
+    [Export(typeof(IAction))]
+    public class CreateConnectorToLogicApp : BaseAction
+    {
+        public override async Task<ActionResponse> ExecuteActionAsync(ActionRequest request)
+        {
+            var azureToken = request.DataStore.GetJson("AzureToken")["access_token"].ToString();
+            var subscription = request.DataStore.GetJson("SelectedSubscription")["SubscriptionId"].ToString();
+            var resourceGroup = request.DataStore.GetValue("SelectedResourceGroup");
+            var location = request.DataStore.GetJson("SelectedLocation")["Name"].ToString();
+            var connectorName = request.DataStore.GetValue("ConnectorName");
+
+
+            SubscriptionCloudCredentials creds = new TokenCloudCredentials(subscription, azureToken);
+            Microsoft.Azure.Management.Resources.ResourceManagementClient client = new ResourceManagementClient(creds);
+            var registeration = await client.Providers.RegisterAsync("Microsoft.Web");
+
+            dynamic payload = new ExpandoObject();
+            payload.properties = new ExpandoObject();
+            payload.properties.displayName = connectorName;
+            payload.properties.api = new ExpandoObject();
+            payload.properties.api.id = $"subscriptions/{subscription}/providers/Microsoft.Web/locations/{location}/managedApis/{connectorName}";
+            payload.location = location;
+
+            HttpResponseMessage connection = await new AzureHttpClient(azureToken, subscription, resourceGroup).ExecuteWithSubscriptionAndResourceGroupAsync(HttpMethod.Put,
+                $"/providers/Microsoft.Web/connections/{connectorName}", "2016-06-01", JsonUtility.GetJsonStringFromObject(payload));
+
+            if (!connection.IsSuccessStatusCode)
+            {
+                return new ActionResponse(ActionStatus.Failure, JsonUtility.GetJObjectFromJsonString(await connection.Content.ReadAsStringAsync()),
+                    null, DefaultErrorCodes.DefaultErrorCode, "Failed to create connection");
+            }
+
+            return new ActionResponse(ActionStatus.Success);
+        }
+    }
+}
