379f0fdfb345f3da733505b6985ad51908ef6eb2
Yasin Shtiui <yashti@microsoft.com>
2016-11-28 16:34:28 -0800

    Translations for azure login page

--- Source/SiteCommon/Web/localization/en-us.ts
+++ Source/SiteCommon/Web/localization/en-us.ts
@@ -1,7 +1,27 @@
 ﻿export const EN_US = {
-    GET_STARTED_WITH: `Get started with`,
-    SHOW_DOWNLOAD: `Since you are copying your data into SQL
+    AZURE_LOGIN_CONNECT: `Connect to Azure`,
+    AZURE_LOGIN_CONNECTION: `Would you like to connect to Azure through an
+        organizational account or a Microsoft account?`,
+    AZURE_LOGIN_DIRECTORY: `Azure Directory:`,
+    AZURE_LOGIN_DIRECTORY_DESCRIPTION: `guide`,
+    AZURE_LOGIN_DIRECTORY_LINK: `http://community.powerbi.com/t5/Power-BI-Solution-Templates/Guide-for-using-Microsoft-accounts-with-Twitter-template/m-p/60088#M31`,
+    AZURE_LOGIN_DIRECTORY_TEXT: `In order to connect to your Microsoft account we will
+        need to know your Azure directory. To figure this out please refer to
+        our `,
+    AZURE_LOGIN_LINK: `https://azure.microsoft.com/en-us/free/`,
+    AZURE_LOGIN_LINK_DESCRIPTION: `Azure subscription trial`,
+    AZURE_LOGIN_MICROSOFT: `Microsoft Account (Hotmail, Outlook, etc.)`,
+    AZURE_LOGIN_ORGANIZATIONAL: `Organizational Account (Work or School)`,
+    AZURE_LOGIN_PRICING_TEXT: `You can learn more about pricing`,
+    AZURE_LOGIN_RESOURCE_GROUP: `Azure Resource Group Name:`,
+    AZURE_LOGIN_SUBTITLE: `To create the solution we will be using a number of Azure
+        Services which require an Azure subscription. Don't worry if you don't
+        have one, you can get started with a free`,
+    COMMON_ADVANCED_OPTIONAL: `Advanced (optional)`,
+    COMMON_HERE: `here`,
+    GETTING_STARTED_SHOW_DOWNLOAD: `Since you are copying your data into SQL
         Server, you will need to use our local installer to set up the
         template. In order to download the solution template installer, please
-        click the Download button below.`
+        click the Download button below.`,
+    GETTING_STARTED_TITLE: `Get started with`
 };
