d566c8deb465a09676ac266bbf94ee83b023aa19
Justyna Lucznik <juluczni@microsoft.com>
2016-11-29 17:40:17 -0800

    News Template - added actions for spinning up cognitive service for Bing + getting cognitive key. Added action for spinning up generic function + deploying generic assets + tests for all

---
+++ Source/Actions/Microsoft.Deployment.Actions.Test/ActionsTest/AzureFunctionsTests.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Deployment.Actions.Test.TestHelpers;
+using Microsoft.Deployment.Common;
+using Microsoft.Deployment.Common.ActionModel;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Deployment.Common.Helpers;
+namespace Microsoft.Deployment.Actions.Test.ActionsTest
+{
+    [TestClass]
+    public class AzureFunctionsTests
+    {
+        [TestMethod]
+        public async Task DeployAzureFunction()
+        {
+            var dataStore = await AAD.GetTokenWithDataStore();
+            var result = await TestHarness.ExecuteActionAsync("Microsoft-GetAzureSubscriptions", dataStore);
+            Assert.IsTrue(result.IsSuccess);
+            var responseBody = JObject.FromObject(result.Body);
+            var subscriptionId = responseBody["value"][0];
+
+            dataStore.AddToDataStore("SelectedSubscription", subscriptionId, DataStoreType.Private);
+            dataStore.AddToDataStore("SelectedResourceGroup", "testing");
+
+            var locationResult = await TestHarness.ExecuteActionAsync("Microsoft-GetLocations", dataStore);
+            Assert.IsTrue(locationResult.IsSuccess);
+            var location = locationResult.Body.GetJObject()["value"][5];
+            dataStore.AddToDataStore("SelectedLocation", location, DataStoreType.Public);
+
+            dataStore.AddToDataStore("DeploymentName", "FunctionDeploymentTest");
+            dataStore.AddToDataStore("FunctionAppHostingPlan", "FunctionPlanName");
+            dataStore.AddToDataStore("SiteName", "UnitTest" + RandomGenerator.GetRandomLowerCaseCharacters(5));
+
+            var response = TestHarness.ExecuteAction("Microsoft-CreateResourceGroup", dataStore);
+
+            response = TestHarness.ExecuteAction("Microsoft-DeployAzureFunction", dataStore);
+            Assert.IsTrue(response.Status == ActionStatus.Success);
+
+        }
+
+        [TestMethod]
+        public async Task DeployAzureFunctionAssets()
+        {
+            var dataStore = await AAD.GetTokenWithDataStore();
+            var result = await TestHarness.ExecuteActionAsync("Microsoft-GetAzureSubscriptions", dataStore);
+            Assert.IsTrue(result.IsSuccess);
+            var responseBody = JObject.FromObject(result.Body);
+            var subscriptionId = responseBody["value"][0];
+
+            dataStore.AddToDataStore("SelectedSubscription", subscriptionId, DataStoreType.Private);
+            dataStore.AddToDataStore("SelectedResourceGroup", "testing");
+
+            var locationResult = await TestHarness.ExecuteActionAsync("Microsoft-GetLocations", dataStore);
+            Assert.IsTrue(locationResult.IsSuccess);
+            var location = locationResult.Body.GetJObject()["value"][5];
+            dataStore.AddToDataStore("SelectedLocation", location, DataStoreType.Public);
+
+            dataStore.AddToDataStore("DeploymentName", "FunctionDeploymentTest");
+            dataStore.AddToDataStore("FunctionAppHostingPlan", "FunctionPlanName");
+            dataStore.AddToDataStore("SiteName", "UnitTestfcrow");
+
+            dataStore.AddToDataStore("FunctionName", "TestA");
+            dataStore.AddToDataStore("FunctionFileName", "TweetFunctionCSharp.cs" );
+
+
+            var response = TestHarness.ExecuteAction("Microsoft-CreateResourceGroup", dataStore);
+
+            response = TestHarness.ExecuteAction("Microsoft-DeployAzureFunctionAssets", dataStore);
+            Assert.IsTrue(response.Status == ActionStatus.Success);
+
+        }
+
+    }
+}
+
--- Source/Actions/Microsoft.Deployment.Actions.Test/ActionsTest/CognitiveServiceTests.cs
+++ Source/Actions/Microsoft.Deployment.Actions.Test/ActionsTest/CognitiveServiceTests.cs
@@ -40,5 +40,83 @@ namespace Microsoft.Deployment.Actions.Test.ActionsTest
             }
         }
 
+        [TestMethod]
+        public async Task DeployCognitiveServiceTest()
+        {
+            var dataStore = await AAD.GetTokenWithDataStore();
+            var result = await TestHarness.ExecuteActionAsync("Microsoft-GetAzureSubscriptions", dataStore);
+            Assert.IsTrue(result.IsSuccess);
+            var responseBody = JObject.FromObject(result.Body);
+            var subscriptionId = responseBody["value"][0];
+
+            dataStore.AddToDataStore("DeploymentName", "CongitiveServiceDeploy", DataStoreType.Private);
+            dataStore.AddToDataStore("SelectedSubscription", subscriptionId, DataStoreType.Private);
+            dataStore.AddToDataStore("SelectedResourceGroup", "testing");
+            dataStore.AddToDataStore("CognitiveServiceName", "TestCognitiveService2");
+            //dataStore.AddToDataStore("CognitiveServiceType", "TextAnalytics");
+            dataStore.AddToDataStore("CognitiveServiceType", "Bing.Search");
+
+            dataStore.AddToDataStore("CognitiveSkuName", "S1");
+            var locationResult = await TestHarness.ExecuteActionAsync("Microsoft-GetLocations", dataStore);
+            Assert.IsTrue(locationResult.IsSuccess);
+            var location = locationResult.Body.GetJObject()["value"][5];
+            dataStore.AddToDataStore("SelectedLocation", location, DataStoreType.Public);
+
+            var response = TestHarness.ExecuteAction("Microsoft-CreateResourceGroup", dataStore);
+            response = TestHarness.ExecuteAction("Microsoft-DeployCognitiveService", dataStore);
+            Assert.IsTrue(response.Status == ActionStatus.Success);
+
+            //if (response.Status == ActionStatus.Success)
+            //{
+            //    response = TestHarness.ExecuteAction("Microsoft-DeleteResourceGroup", dataStore);
+            //    Assert.IsTrue(response.Status == ActionStatus.Success);
+            //}
+        }
+
+        [TestMethod]
+        public async Task GetCognitiveServiceKeys()
+        {
+            var dataStore = await AAD.GetTokenWithDataStore();
+            var result = await TestHarness.ExecuteActionAsync("Microsoft-GetAzureSubscriptions", dataStore);
+            Assert.IsTrue(result.IsSuccess);
+            var responseBody = JObject.FromObject(result.Body);
+            var subscriptionId = responseBody["value"][0];
+
+            dataStore.AddToDataStore("Azure", "SelectedResourceGroup", "testing");
+            dataStore.AddToDataStore("CognitiveService", "CognitiveServiceName", "TestCognitiveService2");
+
+            var response = TestHarness.ExecuteAction("Microsoft-GetCognitiveKey", dataStore);
+            Assert.IsTrue(response.Status == ActionStatus.Success);
+        }
+
+
+        //[TestMethod]
+        //public async Task TestValidCognitiveKey()
+        //{
+        //    var dataStore = await AAD.GetTokenWithDataStore();
+        //    var result = await TestHarness.ExecuteActionAsync("Microsoft-GetAzureSubscriptions", dataStore);
+        //    Assert.IsTrue(result.IsSuccess);
+        //    var responseBody = JObject.FromObject(result.Body);
+        //    var subscriptionId = responseBody["value"][0];
+
+        //    string CognitiveServiceKey = "d917a971e5ef42b3b817608580c77cee";
+        //    dataStore.AddToDataStore("CognitiveServiceKey", CognitiveServiceKey);
+        //    var response = TestService.Instance.ExecuteAction("Microsoft-ValidateCognitiveKey", payload);
+
+        //    Assert.IsTrue(response.Status == ActionStatus.Success);
+        //}
+
+
+        //[TestMethod]
+        //public void TestInvalidCognitiveKey()
+        //{
+        //    var dataStore = GetDataStoreWithTokenSubscriptionLocation();
+        //    dataStore.AddItemToDataStore("CognitiveService", "CognitiveServiceKey", "1223");
+        //    var payload = dataStore.GetDataStore();
+        //    var response = TestService.Instance.ExecuteAction("Microsoft-ValidateCognitiveKey", payload);
+        //    Assert.IsTrue(response.Status == ActionStatus.FailureExpected);
+        //}
+
+
     }
 }
