7780764aaadfa92676056222bb2b95b2b9dfba64
Mohammad Ali (MOHAALI) <mohaali@microsoft.com>
2016-12-01 21:07:19 -0800

    sync previous changes

--- Source/Actions/Microsoft.Deployment.Actions.SQL/CheckSQLVersion.cs
+++ Source/Actions/Microsoft.Deployment.Actions.SQL/CheckSQLVersion.cs
@@ -17,16 +17,21 @@ namespace Microsoft.Deployment.Actions.SQL
             // TODO fix hardcoded string as action here
             string connectionString = request.DataStore.GetAllValues("SqlConnectionString")[1];
 
-            DataTable result = SqlUtility.RunCommand(connectionString, "SELECT SERVERPROPERTY('ProductVersion') AS SqlVersion, SERVERPROPERTY('IsLocalDB') AS IsLocalDB, SERVERPROPERTY('Edition') AS SqlEdition", SqlCommandType.ExecuteWithData);
-            string serverVersion =(string)result.Rows[0]["SqlVersion"];
-            int majorServerVersion = int.Parse(serverVersion.Substring(0, serverVersion.IndexOf('.')));
+            DataTable result = SqlUtility.RunCommand(connectionString, "SELECT @@version AS FullVersion, SERVERPROPERTY('ProductVersion') AS SqlVersion, SERVERPROPERTY('IsLocalDB') AS IsLocalDB, SERVERPROPERTY('Edition') AS SqlEdition", SqlCommandType.ExecuteWithData);
+            string version = (string)result.Rows[0]["FullVersion"];
+            if (version.IndexOf("Azure SQL Data Warehouse", StringComparison.OrdinalIgnoreCase) > -1)
+                return new ActionResponse(ActionStatus.Failure, JsonUtility.GetEmptyJObject(), "SQL_DenyPDW");
 
+            version = (string)result.Rows[0]["SqlVersion"];
+            int majorServerVersion = int.Parse(version.Substring(0, version.IndexOf('.')));
             if (majorServerVersion < 11)
-                return new ActionResponse(ActionStatus.Failure, JsonUtility.GetEmptyJObject(), "SQLVersionTooLow");
+                return new ActionResponse(ActionStatus.Failure, JsonUtility.GetEmptyJObject(), "SQL_VersionTooLow");
 
-            if ( Convert.ToString(result.Rows[0]["SqlEdition"]).IndexOf("Express Edition", StringComparison.OrdinalIgnoreCase) > -1 || Convert.ToInt32(result.Rows[0]["IsLocalDB"]) == 1)
-                return new ActionResponse(ActionStatus.Failure, JsonUtility.GetEmptyJObject(), "SQLDenyLocalAndExpress");
-
+            version = (string)result.Rows[0]["SqlEdition"];
+            if (version.IndexOf("Express Edition", StringComparison.OrdinalIgnoreCase) > -1)
+                return new ActionResponse(ActionStatus.Failure, JsonUtility.GetEmptyJObject(), "SQL_DenyLocalAndExpress");
+            if ((result.Rows[0]["IsLocalDB"] != DBNull.Value) && ((int)result.Rows[0]["IsLocalDB"] == 1))
+                return new ActionResponse(ActionStatus.Failure, JsonUtility.GetEmptyJObject(), "SQL_DenyLocalAndExpress");
 
             return new ActionResponse(ActionStatus.Success, JsonUtility.GetEmptyJObject());
         }
