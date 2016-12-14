c723e930c62f6bef14c69b20b43e5fac53c0c8ed
raymondb <raymondb@microsoft.com>
2016-12-14 14:39:55 -0800

    Track subscription IDs. Do not list deleted or disabled subscriptions

--- Source/Actions/Microsoft.Deployment.Actions.AzureCustom/Common/GetAzureSubscriptions.cs
+++ Source/Actions/Microsoft.Deployment.Actions.AzureCustom/Common/GetAzureSubscriptions.cs
@@ -1,13 +1,14 @@
-﻿using System.ComponentModel.Composition;
-using System.Dynamic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Hyak.Common;
+﻿using Hyak.Common;
 using Microsoft.Azure;
 using Microsoft.Azure.Subscriptions;
+using Microsoft.Azure.Subscriptions.Models;
 using Microsoft.Deployment.Common.ActionModel;
 using Microsoft.Deployment.Common.Actions;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Microsoft.Deployment.Actions.AzureCustom.Common
 {
@@ -19,10 +20,26 @@ namespace Microsoft.Deployment.Actions.AzureCustom.Common
             var azureToken = request.DataStore.GetJson("AzureToken")["access_token"].ToString();
 
             CloudCredentials creds = new TokenCloudCredentials(azureToken);
-            Microsoft.Azure.Subscriptions.SubscriptionClient client = new SubscriptionClient(creds);
-            var subscriptionList = (await client.Subscriptions.ListAsync(new CancellationToken())).Subscriptions.ToList();
             dynamic subscriptionWrapper = new ExpandoObject();
-            subscriptionWrapper.value = subscriptionList;
+
+            List<Subscription> validSubscriptions = new List<Subscription>();
+
+            using (SubscriptionClient client = new SubscriptionClient(creds))
+            {
+                SubscriptionListResult subscriptionList = await client.Subscriptions.ListAsync();
+
+                foreach (Subscription s in subscriptionList.Subscriptions)
+                {
+                    if (s.State.Equals("Disabled", System.StringComparison.OrdinalIgnoreCase) || s.State.Equals("Deleted", System.StringComparison.OrdinalIgnoreCase))
+                        continue;
+                    else
+                        validSubscriptions.Add(s);
+                }
+                
+                subscriptionWrapper.value = validSubscriptions;
+            }
+
+            request.Logger.LogEvent("GetAzureSubscriptions-result", new Dictionary<string, string>() {  { "Subscriptions", string.Join(",", validSubscriptions.Select(p => p.SubscriptionId)) }  });
             return new ActionResponse(ActionStatus.Success, subscriptionWrapper);
         }
     }
