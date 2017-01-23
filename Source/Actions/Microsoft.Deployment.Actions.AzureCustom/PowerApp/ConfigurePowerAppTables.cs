62954c2096a31db31ea70c70cb1f3d78cb8f7828
yashti <yashti@microsoft.com>
2017-01-23 02:33:34 -0800

    Configure Power App Tables

---
+++ Source/Actions/Microsoft.Deployment.Actions.AzureCustom/PowerApp/ConfigurePowerAppTables.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+
+using Microsoft.Deployment.Common.ActionModel;
+using Microsoft.Deployment.Common.Actions;
+using Microsoft.Deployment.Common.Helpers;
+
+namespace Microsoft.Deployment.Actions.AzureCustom.PowerApp
+{
+    [Export(typeof(IAction))]
+    public class ConfigurePowerAppTables : BaseAction
+    {
+        private const string INSERT_INTO_TWITTER_QUERY = "INSERT INTO [dbo].[TwitterQuery] ([Id], [IsAdvanced], [QueryString]) VALUES ({0}, {1}, '{2}')";
+        private const string INSERT_INTO_TWITTER_QUERY_DETAILS = "INSERT INTO [dbo].[TwitterQueryDetails] ([Id], [ReadableId], [Operator], [Operand]) VALUES ({0}, {1}, '{2}', '{3}')";
+        private const string INSERT_INTO_TWITTER_QUERY_READABLE = "INSERT INTO [dbo].[TwitterQueryReadable] ([Id], [QueryId], [QueryReadable], [Query]) VAULES ({0}, {1}, '{2}', '{3}')";
+
+        public override async Task<ActionResponse> ExecuteActionAsync(ActionRequest request)
+        {
+            string searchQuery = request.DataStore.GetValue("SearchQuery") ?? string.Empty;
+            string sqlConnectionString = request.DataStore.GetValueAtIndex("SqlConnectionString", "SqlServerIndex");
+
+            searchQuery = searchQuery.ToLowerInvariant();
+            //SqlUtility.InvokeSqlCommand(sqlConnectionString, string.Format(INSERT_INTO_TWITTER_QUERY, 1, 0, searchQuery), null);
+
+            //string[] conditions = searchQuery.Split(new string[] { "or" }, StringSplitOptions.None);
+
+            //for (int i = 0; i < conditions.Length; i++)
+            //{
+            //    string condition = conditions[i];
+            //}
+
+            //SqlUtility.InvokeSqlCommand(sqlConnectionString, string.Empty, null);
+
+            return new ActionResponse(ActionStatus.Success, JsonUtility.GetEmptyJObject());
+        }
+    }
+}
