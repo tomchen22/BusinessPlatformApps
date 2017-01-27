f86ebaa5d46888c516ae6c15ff635d7269cf7876
Yasin Shtiui <yashti@microsoft.com>
2017-01-26 18:09:33 -0800

    query parse in progress

--- Source/Actions/Microsoft.Deployment.Actions.AzureCustom/PowerApp/ConfigurePowerAppTables.cs
+++ Source/Actions/Microsoft.Deployment.Actions.AzureCustom/PowerApp/ConfigurePowerAppTables.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel.Composition;
+﻿using System;
+using System.ComponentModel.Composition;
 using System.Threading.Tasks;
+using System.Text;
 
 using Microsoft.Deployment.Common.ActionModel;
 using Microsoft.Deployment.Common.Actions;
@@ -19,11 +21,79 @@ namespace Microsoft.Deployment.Actions.AzureCustom.PowerApp
             string searchQuery = request.DataStore.GetValue("SearchQuery") ?? string.Empty;
             string sqlConnectionString = request.DataStore.GetValueAtIndex("SqlConnectionString", "SqlServerIndex");
 
-            bool isAdvanced = false;
+            bool isAdvanced = IsAdvancedTwitterQuery(searchQuery);
 
             SqlUtility.InvokeSqlCommand(sqlConnectionString, string.Format(INSERT_INTO_TWITTER_QUERY, 1, isAdvanced ? 1 : 0, searchQuery, null), null);
 
+            if (!isAdvanced)
+            {
+                string[] conditions = searchQuery.Split(new string[] { "OR" }, StringSplitOptions.None);
+                for (int i = 0; i < conditions.Length; i++)
+                {
+                    string condition = conditions[i].Trim();
+                    StringBuilder sb = new StringBuilder();
+                    for (int j = 0; j < condition.Length; j++)
+                    {
+                        // do stuff
+                    }
+                }
+            }
+
             return new ActionResponse(ActionStatus.Success, JsonUtility.GetEmptyJObject());
+        }
+
+        private bool IsAdvancedTwitterQuery(string query)
+        {
+            bool isAdvanced = false;
+
+            bool isOpenBracket = false;
+            bool isOpenQuote = false;
+
+            for (int i = 0; i < query.Length && !isAdvanced; i++)
+            {
+                switch (query[i])
+                {
+                    case '"':
+                        isOpenQuote = !isOpenQuote;
+                        break;
+                    case '(':
+                        if (!isOpenQuote)
+                        {
+                            if (isOpenBracket)
+                            {
+                                isAdvanced = true;
+                            }
+                            else
+                            {
+                                isOpenBracket = true;
+                            }
+                        }
+                        break;
+                    case ')':
+                        if (!isOpenQuote)
+                        {
+                            if (isOpenBracket)
+                            {
+                                isOpenBracket = false;
+                            }
+                            else
+                            {
+                                isAdvanced = true;
+                            }
+                        }
+                        break;
+                    case 'O':
+                        if (!isOpenQuote && i < query.Length - 1 && query[i + 1] == 'R')
+                        {
+                            isAdvanced = isOpenBracket;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return isAdvanced;
         }
     }
 }
