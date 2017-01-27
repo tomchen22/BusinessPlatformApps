54b004ee77bd8249012e5dc0967647fdf8c89cfb
yashti <yashti@microsoft.com>
2017-01-26 23:51:57 -0800

    full twitter query parse and deployment

--- Source/Actions/Microsoft.Deployment.Actions.AzureCustom/PowerApp/ConfigurePowerAppTables.cs
+++ Source/Actions/Microsoft.Deployment.Actions.AzureCustom/PowerApp/ConfigurePowerAppTables.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Threading.Tasks;
 using System.Text;
@@ -12,9 +13,13 @@ namespace Microsoft.Deployment.Actions.AzureCustom.PowerApp
     [Export(typeof(IAction))]
     public class ConfigurePowerAppTables : BaseAction
     {
-        private const string INSERT_INTO_TWITTER_QUERY = "INSERT INTO [pbist_twitter].[twitter_query] ([Id], [IsAdvanced], [QueryString], [TweetId]) VALUES ({0}, {1}, '{2}', {3})";
+        private const string INSERT_INTO_TWITTER_QUERY = "INSERT INTO [pbist_twitter].[twitter_query] ([Id], [IsAdvanced], [QueryString]) VALUES ({0}, {1}, '{2}')";
         private const string INSERT_INTO_TWITTER_QUERY_DETAILS = "INSERT INTO [pbist_twitter].[twitter_query_details] ([Id], [ReadableId], [Operator], [Operand]) VALUES ({0}, {1}, '{2}', '{3}')";
         private const string INSERT_INTO_TWITTER_QUERY_READABLE = "INSERT INTO [pbist_twitter].[twitter_query_readable] ([Id], [QueryId], [QueryReadable], [Query]) VAULES ({0}, {1}, '{2}', '{3}')";
+
+        private const string TWITTER_OPERATOR_AND = " AND ";
+        private const string TWITTER_OPERATOR_CONTAINS = "Contains";
+        private const string TWITTER_OPERATOR_DOES_NOT_CONTAIN = "Does not contain";
 
         public override async Task<ActionResponse> ExecuteActionAsync(ActionRequest request)
         {
@@ -23,23 +28,109 @@ namespace Microsoft.Deployment.Actions.AzureCustom.PowerApp
 
             bool isAdvanced = IsAdvancedTwitterQuery(searchQuery);
 
-            SqlUtility.InvokeSqlCommand(sqlConnectionString, string.Format(INSERT_INTO_TWITTER_QUERY, 1, isAdvanced ? 1 : 0, searchQuery, null), null);
+            SqlUtility.InvokeSqlCommand(sqlConnectionString, string.Format(INSERT_INTO_TWITTER_QUERY, 1, isAdvanced ? 1 : 0, searchQuery), null);
 
             if (!isAdvanced)
             {
-                string[] conditions = searchQuery.Split(new string[] { "OR" }, StringSplitOptions.None);
-                for (int i = 0; i < conditions.Length; i++)
+                List<string> conditions = SplitQueryByOr(searchQuery);
+                for (int i = 0; i < conditions.Count; i++)
                 {
-                    string condition = conditions[i].Trim();
-                    StringBuilder sb = new StringBuilder();
-                    for (int j = 0; j < condition.Length; j++)
+                    string readableQuery = GetReadableQueryPart(conditions[i]);
+                    SqlUtility.InvokeSqlCommand(sqlConnectionString, string.Format(INSERT_INTO_TWITTER_QUERY_READABLE, i + 1, 1, readableQuery, conditions[i]), null);
+
+                    string[] details = readableQuery.Split(new string[] { TWITTER_OPERATOR_AND }, StringSplitOptions.None);
+                    for (int j = 0; j < details.Length; j++)
                     {
-                        // do stuff
+                        string detail = details[j];
+                        if (detail[0] == 'C')
+                        {
+                            SqlUtility.InvokeSqlCommand(sqlConnectionString, string.Format(INSERT_INTO_TWITTER_QUERY_DETAILS, j + 1, i + 1, TWITTER_OPERATOR_CONTAINS, detail.Substring(TWITTER_OPERATOR_CONTAINS.Length + 2).TrimEnd('"')), null);
+                        }
+                        else
+                        {
+                            SqlUtility.InvokeSqlCommand(sqlConnectionString, string.Format(INSERT_INTO_TWITTER_QUERY_DETAILS, j + 1, i + 1, TWITTER_OPERATOR_DOES_NOT_CONTAIN, detail.Substring(TWITTER_OPERATOR_DOES_NOT_CONTAIN.Length + 2).TrimEnd('"')), null);
+                        }
                     }
                 }
             }
 
             return new ActionResponse(ActionStatus.Success, JsonUtility.GetEmptyJObject());
+        }
+
+        private string GetReadableQueryPart(string query)
+        {
+            StringBuilder sbPart = new StringBuilder();
+            StringBuilder sbRead = new StringBuilder();
+
+            bool isOpenQuote = false;
+            bool isTerminated = false;
+
+            for (int i = 0; i < query.Length; i++)
+            {
+                switch (query[i])
+                {
+                    case '"':
+                        if (isOpenQuote)
+                        {
+                            isTerminated = true;
+                        }
+                        isOpenQuote = !isOpenQuote;
+                        break;
+                    case ' ':
+                        if (isOpenQuote)
+                        {
+                            sbPart.Append(query[i]);
+                        }
+                        else
+                        {
+                            isTerminated = true;
+                        }
+                        break;
+                    case '(':
+                        if (isOpenQuote)
+                        {
+                            sbPart.Append(query[i]);
+                        }
+                        break;
+                    case ')':
+                        if (isOpenQuote)
+                        {
+                            sbPart.Append(query[i]);
+                        }
+                        else
+                        {
+                            isTerminated = true;
+                        }
+                        break;
+                    default:
+                        sbPart.Append(query[i]);
+                        break;
+                }
+
+                if (isTerminated || i == query.Length - 1)
+                {
+                    string read = sbPart.ToString();
+                    if (read.Length > 0)
+                    {
+                        if (read[0] == '-')
+                        {
+                            sbRead.Append($"{TWITTER_OPERATOR_DOES_NOT_CONTAIN} \"{read.Substring(1)}\"");
+                        }
+                        else
+                        {
+                            sbRead.Append($"{TWITTER_OPERATOR_CONTAINS} \"{read}\"");
+                        }
+                        if (i != query.Length - 1)
+                        {
+                            sbRead.Append(TWITTER_OPERATOR_AND);
+                        }
+                    }
+                    sbPart.Clear();
+                    isTerminated = false;
+                }
+            }
+
+            return sbRead.ToString();
         }
 
         private bool IsAdvancedTwitterQuery(string query)
@@ -94,6 +185,46 @@ namespace Microsoft.Deployment.Actions.AzureCustom.PowerApp
             }
 
             return isAdvanced;
+        }
+
+        private List<string> SplitQueryByOr(string query)
+        {
+            List<string> parts = new List<string>();
+            StringBuilder sb = new StringBuilder();
+
+            bool isOpenQuote = false;
+
+            for (int i = 0; i < query.Length; i++)
+            {
+                switch (query[i])
+                {
+                    case 'O':
+                        if (!isOpenQuote && i < query.Length - 1 && query[i + 1] == 'R')
+                        {
+                            if (sb.Length > 0)
+                            {
+                                parts.Add(sb.ToString().Trim());
+                                sb.Clear();
+                            }
+                            i++;
+                        }
+                        break;
+                    case '"':
+                        isOpenQuote = !isOpenQuote;
+                        sb.Append(query[i]);
+                        break;
+                    default:
+                        sb.Append(query[i]);
+                        break;
+                }
+            }
+
+            if (sb.Length > 0)
+            {
+                parts.Add(sb.ToString().Trim());
+            }
+
+            return parts;
         }
     }
 }
