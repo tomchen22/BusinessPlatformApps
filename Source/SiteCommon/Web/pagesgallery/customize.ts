562436eba725cce7907c49cdfd978b14b9ecd39d
Catalin Doroftei <catdor@microsoft.com>
2017-02-07 14:03:23 -0800

    modified customize

--- Source/SiteCommon/Web/pagesgallery/customize.ts
+++ Source/SiteCommon/Web/pagesgallery/customize.ts
@@ -4,7 +4,7 @@ import { ViewModelBase } from '../services/viewmodelbase';
 export class Customize extends ViewModelBase {
     actuals: string = 'Closed opportunities';
     baseUrl: string = '';
-    fiscalMonth: string = 'January';
+    fiscalMonth: number = 1;
     emails: string = '';
     emailRegex: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     pipelineFrequency: string = '';
@@ -88,7 +88,7 @@ export class Customize extends ViewModelBase {
         this.MS.DataStore.addToDataStoreWithCustomRoute('CustomizeActuals', 'SqlGroup', 'data', DataStoreType.Public);
         this.MS.DataStore.addToDataStoreWithCustomRoute('CustomizeActuals', 'SqlSubGroup', 'actual_sales', DataStoreType.Public);
         this.MS.DataStore.addToDataStoreWithCustomRoute('CustomizeActuals', 'SqlEntryName', 'enabled', DataStoreType.Public);
-        this.MS.DataStore.addToDataStoreWithCustomRoute('CustomizeActuals', 'SqlEntryValue', this.actuals ? 1 : 0, DataStoreType.Public);
+        this.MS.DataStore.addToDataStoreWithCustomRoute('CustomizeActuals', 'SqlEntryValue', this.actuals === "Closed opportunities" ? 0 : 1, DataStoreType.Public);
 
         this.MS.DataStore.addToDataStoreWithCustomRoute('CustomizeBaseUrl', 'SqlGroup', 'SolutionTemplate', DataStoreType.Public);
         this.MS.DataStore.addToDataStoreWithCustomRoute('CustomizeBaseUrl', 'SqlSubGroup', 'SalesManagement', DataStoreType.Public);
