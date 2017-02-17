0ed101537e761a4a1266279fe99b04a678f5e41f
Justyna <Justyna@DESKTOP-424UD00>
2017-02-16 20:11:59 -0800

    Bringing back previous power app changes

--- Source/Apps/Microsoft/Released/Microsoft-TwitterTemplate/Service/Database/LogicApps/00 content check.sql
+++ Source/Apps/Microsoft/Released/Microsoft-TwitterTemplate/Service/Database/LogicApps/00 content check.sql
@@ -1,5 +1,5 @@
 SELECT Count(*) AS ExistingObjectCount
 FROM   information_schema.tables
 WHERE  ( table_schema = 'pbist_twitter' AND
-            table_name IN ('configuration', 'date', 'tweets_processed', 'tweets_normalized', 'hashtag_slicer', 'mention_slicer', 'entity_graph', 'authorhashtag_graph', 'authormention_graph', 'entities', 'entities2')
+            table_name IN ('configuration', 'date', 'tweets_processed', 'tweets_normalized', 'hashtag_slicer', 'mention_slicer', 'entity_graph', 'authorhashtag_graph', 'authormention_graph', 'entities', 'entities2', 'twitter_query')
         );
