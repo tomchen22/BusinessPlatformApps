57b3d453663d589532aa3191adb7e7962e03207b
Cat <catdor@microsoft.com>
2017-02-17 11:48:14 -0800

    removed actual_sales traces from code and customize page

--- Source/Apps/Microsoft/Released/Microsoft-SalesforceSalesManagement/Service/Database/ADF/60 data.sql
+++ Source/Apps/Microsoft/Released/Microsoft-SalesforceSalesManagement/Service/Database/ADF/60 data.sql
@@ -2941,7 +2941,6 @@ go
 /************************************
 * Configuration values              *
 *************************************/
-INSERT smgt.[configuration] (configuration_group, configuration_subgroup, [name], [value]) VALUES (N'data', N'actual_sales', N'enabled', N'0');
 INSERT smgt.[configuration] (configuration_group, configuration_subgroup, [name], [value]) VALUES (N'SolutionTemplate', N'SalesManagement', N'version', N'1.0');
 INSERT smgt.[configuration] (configuration_group, configuration_subgroup, [name], [value], [visible]) VALUES (N'SolutionTemplate', N'SalesManagement', N'FiscalMonthStart', N'1', 1);
 go
