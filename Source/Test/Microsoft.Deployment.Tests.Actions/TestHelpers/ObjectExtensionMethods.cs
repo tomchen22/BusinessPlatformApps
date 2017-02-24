1e0224300f756d1e562e7ad6c932b61e08009c6f
Mohammad Ali (MOHAALI) <mohaali@microsoft.com>
2017-02-24 14:27:37 -0800

    improved the testing experience

---
+++ Source/Test/Microsoft.Deployment.Tests.Actions/TestHelpers/ObjectExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Deployment.Tests.Actions.TestHelpers
+{
+    public static class ObjectExtensionMethods
+    {
+        public static JObject GetJObject(this object val)
+        {
+            return JObject.FromObject(val);
+        }
+    }
+}
---
+++ Source/Test/Microsoft.Deployment.Tests.Actions/TestHelpers/TestManager.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Deployment.Common.ActionModel;
+using Microsoft.Deployment.Common.AppLoad;
+using Microsoft.Deployment.Common.Controller;
+using Microsoft.Deployment.Common.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace Microsoft.Deployment.Tests.Actions.TestHelpers
+{
+    [TestClass]
+    public class TestManager
+    {
+        public static string RandomString = RandomGenerator.GetRandomLowerCaseCharacters(8);
+        public static string ResourceGroup = Environment.MachineName + "test";
+
+        private static CommonController Controller { get; set; }
+        public static string TemplateName = "Microsoft-NewsTemplateTest";
+
+        private static async Task<DataStore> GetDataStoreWithToken()
+        {
+            // Read from file DataStore
+            if (File.Exists("datastore.json"))
+            {
+                string filecontents = File.ReadAllText("datastore.json");
+                return JsonConvert.DeserializeObject<DataStore>(filecontents);
+            }
+
+            // Refresh Token - if refresh faile
+
+            // If not found or refresh failed prompt
+            Credential.Load();
+            var dataStore = await AAD.GetUserTokenFromPopup();
+            var subscriptionResult = await TestManager.ExecuteActionAsync("Microsoft-GetAzureSubscriptions", dataStore);
+            Assert.IsTrue(subscriptionResult.IsSuccess);
+            var subscriptionId = subscriptionResult.Body.GetJObject()["value"].SingleOrDefault(p => p["DisplayName"].ToString().StartsWith("PBI_"));
+            dataStore.AddToDataStore("SelectedSubscription", subscriptionId, DataStoreType.Public);
+
+            var locationResult = await TestManager.ExecuteActionAsync("Microsoft-GetLocations", dataStore);
+            var location = locationResult.Body.GetJObject()["value"][5];
+            dataStore.AddToDataStore("SelectedLocation", location, DataStoreType.Public);
+            dataStore.AddToDataStore("SelectedResourceGroup", ResourceGroup);
+
+            var resourceGroupResult = await TestManager.ExecuteActionAsync("Microsoft-CreateResourceGroup", dataStore);
+            Assert.IsTrue(resourceGroupResult.IsSuccess);
+
+            System.IO.File.WriteAllText("datastore.json", JsonUtility.GetJObjectFromObject(dataStore).ToString());
+            return dataStore;
+        }
+
+        public static async Task<DataStore> GetDataStore()
+        {
+            var dataStore = await GetDataStoreWithToken();
+            return dataStore;
+        }
+
+        [AssemblyInitialize()]
+        public static void AssemblyInit(TestContext context)
+        {
+            AppFactory factory = new AppFactory(true);
+            CommonControllerModel model = new CommonControllerModel()
+            {
+                AppFactory = factory,
+                AppRootFilePath = factory.AppPath,
+                SiteCommonFilePath = factory.SiteCommonPath,
+                ServiceRootFilePath = factory.SiteCommonPath + "../",
+                Source = "TEST",
+            };
+
+            Controller = new CommonController(model);
+            Credential.Load();
+        }
+
+        [AssemblyCleanup()]
+        public static async void AssemblyCleanup()
+        {
+        }
+
+        public static ActionResponse ExecuteAction(string actionName, DataStore datastore)
+        {
+            UserInfo info = new UserInfo();
+            info.ActionName = actionName;
+            info.AppName = TemplateName;
+            info.WebsiteRootUrl = "https://unittest";
+            return Controller.ExecuteAction(info, new ActionRequest() { DataStore = datastore }).Result;
+        }
+
+        public static async Task<ActionResponse> ExecuteActionAsync(string actionName, DataStore datastore, string templateName = "Microsoft-NewsTemplateTest")
+        {
+            UserInfo info = new UserInfo();
+            info.ActionName = actionName;
+            info.AppName = templateName;
+            info.WebsiteRootUrl = "https://unittest";
+            return await Controller.ExecuteAction(info, new ActionRequest() { DataStore = datastore });
+        }
+    }
+}
