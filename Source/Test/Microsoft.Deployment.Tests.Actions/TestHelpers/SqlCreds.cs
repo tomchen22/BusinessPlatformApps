68a3961f58a34f47d2bbede14c0312222a9610d4
Mohammad Ali (MOHAALI) <mohaali@microsoft.com>
2017-02-27 15:06:07 -0800

    Made modifications to refresh token code and fixed AS Bug

--- Source/Test/Microsoft.Deployment.Tests.Actions/TestHelpers/SqlCreds.cs
+++ Source/Test/Microsoft.Deployment.Tests.Actions/TestHelpers/SqlCreds.cs
@@ -1,4 +1,10 @@
-﻿namespace Microsoft.Deployment.Tests.Actions.TestHelpers
+﻿using System.Dynamic;
+using Microsoft.Deployment.Common.ActionModel;
+using Microsoft.Deployment.Common.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Deployment.Tests.Actions.TestHelpers
 {
     public class SqlCreds
     {
@@ -9,5 +15,25 @@
         public string Password { get; set; }
 
         public string Database { get; set; }
+
+        public static string GetSqlPagePayload(string database)
+        {
+            var dataStore = new DataStore();
+
+            dynamic sqlPayload = new ExpandoObject();
+            sqlPayload.SqlCredentials = new ExpandoObject();
+            sqlPayload.SqlCredentials.Server = Credential.Instance.Sql.Server;
+            sqlPayload.SqlCredentials.AuthType = "azuresql";
+            sqlPayload.SqlCredentials.User = Credential.Instance.Sql.Username;
+            sqlPayload.SqlCredentials.Password = Credential.Instance.Sql.Password;
+            sqlPayload.SqlCredentials.Database = database;
+
+            dataStore.AddObjectDataStore("SqlCredentials", JsonUtility.GetJObjectFromObject(sqlPayload), DataStoreType.Any);
+
+            ActionResponse sqlResponse = TestManager.ExecuteAction("Microsoft-GetSqlConnectionString", dataStore);
+            Assert.IsTrue(sqlResponse.Status == ActionStatus.Success);
+
+            return (sqlResponse.Body as JObject)["value"].ToString();
+        }
     }
 }
