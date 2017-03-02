caca194f053391b637e2fbab672d02a18ed6346c
Cat <catdor@microsoft.com>
2017-03-02 12:00:21 -0800

    wip

---
+++ Source/Test/Microsoft.Deployment.Tests.Actions/OnPrem/VersionTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Deployment.Common.ActionModel;
+using Microsoft.Deployment.Tests.Actions.TestHelpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Deployment.Tests.Actions.OnPrem
+{
+    [TestClass]
+    [Ignore]
+    public class VersionTests
+    {
+        [TestMethod]
+        public void CheckVersion()
+        {
+            var dataStore = new DataStore();
+            var response = TestManager.ExecuteActionAsync("Microsoft-CheckVersion", dataStore).Result;
+        }
+    }
+}
